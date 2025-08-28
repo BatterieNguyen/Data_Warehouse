c700a970655e1d429542591d21c156fcc43f1f6b
Batterie Nguyen <127811519+BatterieNguyen@users.noreply.github.com>
2025-08-28 15:09:44 +0700

    Update init_databse.sql

--- init_databse.sql
+++ init_databse.sql
@@ -35,8 +35,8 @@ Create Bronze Tables
 ============================================================
 Purpose:
   This query creates tables in the 'bronze' schema, dropping existing tables if they already exist.
-
-IF OBJECT_ID('bronze.crm_cust_info, 'U') IS NOT NULL
+*/
+IF OBJECT_ID ('bronze.crm_cust_info', 'U') IS NOT NULL -- 'U' stands for user
   DROP TABLE bronze.crm_cust_info;
 GO
 
@@ -51,7 +51,9 @@ CREATE TABLE bronze.crm_cust_info (
 );
 GO
 
-
+IF OBJECT_ID ('bronze.crm_prd_info', 'U') IS NOT NULL
+  DROP TABLE bronze.crm_prd_info;
+GO
 
 CREATE TABLE bronze.crm_prd_info (
   prd_id INT,
@@ -62,6 +64,10 @@ CREATE TABLE bronze.crm_prd_info (
   prd_start_dt DATETIME,
   prd_end_dt DATETIME
 );
+
+IF OBJECT_ID ('bronze.crm_sales_details', 'U') IS NOT NULL
+  DROP TABLE bronze.crm_sales_details;
+GO
 
 CREATE TABLE bronze.crm_sales_details (
   sls_ord_num   NVARCHAR(50),
@@ -76,12 +82,20 @@ CREATE TABLE bronze.crm_sales_details (
 );
 GO
 
+IF OBJECT_ID ('bronze.erp_loc_a101', 'U') IS NOT NULL
+  DROP TABLE bronze.erp_loc_a101;
+GO
+
 CREATE TABLE bronze.erp_loc_a101 (
   cid    NVARCHAR(50),
   cntry  NVARCHAR(50)
 );
 GO
 
+IF OBJECT_ID ('bronze.erp_cust_az12', 'U') IS NOT NULL
+  DROP TABLE bronze.erp_cust_az12;
+GO
+  
 CREATE TABLE bronze.erp_cust_az12 (
   cid    NVARCHAR(50),
   bdate  DATE,
@@ -89,7 +103,11 @@ CREATE TABLE bronze.erp_cust_az12 (
 );
 GO
 
-CREATE TABLE bronze.cat_g1v2 (
+IF OBJECT_ID ('bronze.erp_cat_g1v2', 'U') IS NOT NULL
+  DROP TABLE bronze.erp_cat_g1v2;
+GO
+
+CREATE TABLE bronze.erp_cat_g1v2 (
   id          NVARCHAR(50),
   cat         NVARCHAR(50),
   subcat      NVARCHAR(50),
