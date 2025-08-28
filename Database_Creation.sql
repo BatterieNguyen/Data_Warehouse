3eb4b0a667df5325e516356a0ae1541d85b20d02
Batterie Nguyen <127811519+BatterieNguyen@users.noreply.github.com>
2025-08-28 14:19:35 +0700

    Create Database_Creation.sql

---
+++ Database_Creation.sql
@@ -0,0 +1,11 @@
+USE master;
+
+CREATE DATABASE DataWarehouse;
+-------------------------------------------------------
+USE DataWarehouse;
+
+CREATE SCHEMA bronze;
+GO // Separate batches when working with multiple SQL Statement
+CREATE SCHEMA silver;
+
+
